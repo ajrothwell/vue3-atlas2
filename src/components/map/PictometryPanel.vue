bbe4cb5bc1e45a8c30eea3a0d8f7f6da1c4cd062
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-05-03 11:55:50 -0400

    sets up pictometry to get credentials when we have correct client id and secret

--- src/components/map/PictometryPanel.vue
+++ src/components/map/PictometryPanel.vue
@@ -7,9 +7,12 @@ import { Base64 } from 'js-base64';
 import qs from 'qs';
 const clientId = import.meta.env.VITE_PICTOMETRY_API_KEY;
 const clientSecret = import.meta.env.VITE_PICTOMETRY_SECRET_KEY;
-const basicAuth = 'Basic ' + Base64.encode(clientId + ':' + clientSecret);
+// const basicAuth = 'Basic ' + Base64.encode(clientId + ':' + clientSecret);
+const basicAuth = 'Basic ' + btoa(clientId + ':' + clientSecret);
 const data = { 'grant_type': 'client_credentials' };
-const url = 'https://api.eagleview.com/auth-service/v1/token HTTP/1.1';
+const data2 = qs.stringify(data);
+const url = 'https://api.eagleview.com/auth-service/v1/token';
+// const url = 'https://apicenter.eagleview.com/oauth2/v1/token';
 const options = {
   method: 'POST',
   headers: {
@@ -24,7 +27,7 @@ const options = {
 
 // const params = new URLSearchParams();
 // params.append('grant_type', 'client_credentials');
-console.log('clientId:', clientId, 'clientSecret:', clientSecret, 'basicAuth:', basicAuth);
+console.log('clientId:', clientId, 'clientSecret:', clientSecret, 'basicAuth:', basicAuth, 'data2:', data2);
 
 onMounted( async() => {
   // const params = {
@@ -36,12 +39,13 @@ onMounted( async() => {
   //   'Content-Type': 'application/x-www-form-urlencoded',
   // }
   const response = await axios(options);
-  // const response = await axios.post('https://api.eagleview.com/auth-service/v1/token HTTP/1.1', params, headers);
-  console.log('response:', response);
-  const map = new window.ev.EmbeddedExplorer().mount('pictometry', { authToken: response.data.access_token });
+  // // const response = await axios.post('https://api.eagleview.com/auth-service/v1/token HTTP/1.1', params, headers);
+  // console.log('response:', response);
+  // const map = new window.ev.EmbeddedExplorer().mount('pictometry', { apiKey:'D33AB30CEC52FC1B2EB1F4BED47C47A1' });
+  // const map = new window.ev.EmbeddedExplorer().mount('pictometry', { authToken: response.data.access_token });
 })
 </script>
 
 <template>
-  <div id='pictometry' style="height: 100%; width: 50%;"></div>
+  <div id='pictometry' style="height: 100%; width: 25%;"></div>
 </template>
