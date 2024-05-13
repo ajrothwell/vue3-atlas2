562b90460cce2e7fbdb048e6623b99b8e80e67a1
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-05-13 10:22:33 -0400

    makes multiPolygon work for dor parcels, which is BETTER than the old atlas

--- src/router/index.js
+++ src/router/index.js
@@ -35,15 +35,16 @@ const getAddressAndPutInStore = async(address) => {
   MainStore.setCurrentAddress(currentAddress);
 }
 
-const getParcelAndPutInStore = async(lng, lat) => {
+const getParcelsAndPutInStore = async(lng, lat) => {
   let currentAddress;
   const MainStore = useMainStore();
   MainStore.selectedParcelId = null;
   let currentTopic = MainStore.currentTopic;
   const parcelLayer = $config.parcelLayerForTopic[currentTopic];
   const ParcelsStore = useParcelsStore();
-  await ParcelsStore.fillParcelDataByLngLat(lng, lat, parcelLayer);
-  if (!ParcelsStore[parcelLayer].features[0]) {
+  await ParcelsStore.fillParcelDataByLngLat(lng, lat, 'pwd');
+  await ParcelsStore.fillParcelDataByLngLat(lng, lat, 'dor');
+  if (!ParcelsStore.pwd.features[0] && !ParcelsStore.dor.features[0]) {
     router.push({ name: 'not-found' });
     return;
   }
@@ -83,12 +84,8 @@ const dataFetch = async(to, from) => {
 
   console.log('dataFetch is still going after address');
   // GET PARCELS AND DATA FOR TOPIC
-  // const ParcelsStore = useParcelsStore();
-  // if (!ParcelsStore.pwd.features) {
-  await ParcelsStore.fillPwdParcelData();
-  // }
-  // if (MainStore.lastSearchMethod === 'address' || !ParcelsStore.dor.features) {
-  if (MainStore.lastSearchMethod === 'address' || $config.parcelLayerForTopic[topic] === 'pwd') {
+  if (MainStore.lastSearchMethod === 'address') { 
+    await ParcelsStore.fillPwdParcelData();
     await ParcelsStore.fillDorParcelData();
   } 
   const CondosStore = useCondosStore();
@@ -207,7 +204,7 @@ const router = createRouter({
           await getAddressAndPutInStore(address);
         } else if (lat && lng) {
           MainStore.setLastSearchMethod('mapClick');
-          await getParcelAndPutInStore(lng, lat);
+          await getParcelsAndPutInStore(lng, lat);
         }
         routeApp(router);
       },
