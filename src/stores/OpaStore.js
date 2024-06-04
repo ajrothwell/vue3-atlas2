004b9b19d4fe2b74ef2698dff10b1a2714f08571
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-06-04 11:11:28 -0400

    adds try/catch to opa query, makes keeping building highlighted when you reopen L&I work

--- src/stores/OpaStore.js
+++ src/stores/OpaStore.js
@@ -12,10 +12,18 @@ export const useOpaStore = defineStore('OpaStore', {
   },
   actions: {
     async fillOpaData() {
-      const GeocodeStore = useGeocodeStore();
-      const OpaNum = GeocodeStore.aisData.features[0].properties.opa_account_num;
-      const response = await fetch(`https://phl.carto.com/api/v2/sql?q=select+*+from+opa_properties_public+where+parcel_number+%3D+%27${OpaNum}%27`);
-      this.opaData = await response.json()
+      try {
+        const GeocodeStore = useGeocodeStore();
+        const OpaNum = GeocodeStore.aisData.features[0].properties.opa_account_num;
+        const response = await fetch(`https://phl.carto.com/api/v2/sql?q=select+*+from+opa_properties_public+where+parcel_number+%3D+%27${OpaNum}%27`);
+        if (response.ok) {
+          this.opaData = await response.json()
+        } else {
+          console.warn('opaData - await resolved but HTTP status was not successful')
+        }
+      } catch {
+        console.error('opaData - await never resolved, failed to fetch address data')
+      }
     },
   },
   // keeping formatting getters here in the store only works if the data is not looped
