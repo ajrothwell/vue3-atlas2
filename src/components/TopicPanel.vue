5e50972476a33cf24302207f969fe228c17d4f43
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-06-20 13:36:58 -0400

    changes front page content for CityAtlas push

--- src/components/TopicPanel.vue
+++ src/components/TopicPanel.vue
@@ -21,6 +21,9 @@ import Voting from '@/components/topics/Voting.vue';
 import NearbyActivity from '@/components/topics/nearbyActivity/NearbyActivity.vue';
 
 import { useRoute } from 'vue-router';
+
+const version = import.meta.env.VITE_VERSION;
+
 const route = useRoute();
 
 const address = computed(() => MainStore.currentAddress);
@@ -43,7 +46,7 @@ const zipCode = computed(() => {
       
   <!-- FRONT PAGE CONTENT -->
   <div
-    v-if="route.name == 'home'"
+    v-if="route.name == 'home' && version == 'atlas'"
     id="topic-panel-set-content"
   >
     <div v-if="MainStore.fullScreenTopicsEnabled">
@@ -61,6 +64,30 @@ const zipCode = computed(() => {
       <li>Research real estate information including property values, zoning, and document archives</li>
       <li>Get easy access to a variety of hard-to-find City resources</li>
       <li>View recent activity around your address, such as crimes, 311 service requests, and more</li>
+      <li>Explore historical imagery and maps</li>
+    </ul>
+    <br>
+    <p>To get started, click anywhere on the map, or type an address, intersection, property assessment account number, or Department of Records Map Registry number into the search box.</p>
+  </div>
+
+  <div
+    v-if="route.name == 'home' && version == 'cityatlas'"
+    id="topic-panel-set-content"
+  >
+    <div v-if="MainStore.fullScreenTopicsEnabled">
+      <address-search-control :input-id="'address-bar-search-input'" />
+    </div>
+    <h1 class="subtitle is-3">
+      CityAtlas connects you with information about any address in the city.
+    </h1>
+    <p class="subtitle is-4">
+      Here are some things you can do with CityAtlas:
+    </p>
+    <ul class="bullet-list">
+      <li>Research real estate information including property values, zoning, and document archives</li>
+      <li>Get the history of permits, licenses, and inspections at any address</li>
+      <li>Easily access high-resolution street-level and aerial imagery</li>
+      <li>View activity around an address, including vacancy, crime, 311 service requests, and more</li>
       <li>Explore historical imagery and maps</li>
     </ul>
     <br>
