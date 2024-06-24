8426398f0034a423bc6f02c756a0821575e99064
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-06-24 16:50:18 -0400

    adds stormwater horizontal table for accounts

--- src/components/topics/cityAtlas/Stormwater.vue
+++ src/components/topics/cityAtlas/Stormwater.vue
@@ -10,12 +10,32 @@ import VerticalTable from '@/components/VerticalTable.vue';
 import useTransforms from '@/composables/useTransforms';
 const { thousandsPlace } = useTransforms();
 
+import CustomPaginationLabels from '@/components/pagination/CustomPaginationLabels.vue';
+import useTables from '@/composables/useTables';
+const { paginationOptions } = useTables();
+
 const stormwaterData = computed(() => {
   let data;
   if (StormwaterStore.stormwaterData && Object.keys(StormwaterStore.stormwaterData).length) {
     data = StormwaterStore.stormwaterData
   }
   return data;
+})
+
+const accounts = computed(() => {
+  let data;
+  if (StormwaterStore.stormwaterData && Object.keys(StormwaterStore.stormwaterData).length) {
+    data = StormwaterStore.stormwaterData.Accounts
+  }
+  return data;
+})
+
+const accountsLength = computed(() => {
+  let value;
+  if (StormwaterStore.stormwaterData && Object.keys(StormwaterStore.stormwaterData).length) {
+    value = StormwaterStore.stormwaterData.Accounts.length;
+  }
+  return value;
 })
 
 const stormwaterCapData = computed(() => {
@@ -64,6 +84,38 @@ const vertTableData = computed(() => {
   }
 })
 
+const accountsTableData = computed(() => {
+  return {
+    columns: [
+    {
+        label: 'Account #',
+        field: 'AccountNumber',
+      },
+      {
+        label: 'Customer',
+        field: 'CustomerName',
+      },
+      {
+        label: 'Status',
+        field: 'AcctStatus'
+      },
+      {
+        label: 'Service Type',
+        field: 'ServiceTypeLabel',
+      },
+      {
+        label: 'Size',
+        field: 'MeterSize',
+      },
+      {
+        label: 'Stormwater',
+        field: 'StormwaterStatus'
+      },
+    ],
+    rows: accounts.value || [],
+  }
+})
+
 </script>
 
 <template>
@@ -94,5 +146,57 @@ const vertTableData = computed(() => {
     table-id="stormwaterTable"
     :data="vertTableData"
   />
+
+  <div class="data-section">
+    <h5 class="subtitle is-5 table-title">
+      Accounts
+      <font-awesome-icon
+        v-if="StormwaterStore.loadingStormwaterData"
+        icon="fa-solid fa-spinner"
+        spin
+      />
+      <span v-else>({{ accountsLength }})</span>
+    </h5>
+    <div
+      v-if="accountsTableData.rows"
+      class="horizontal-table"
+    >
+
+      <vue-good-table
+        id="accounts"
+        :columns="accountsTableData.columns"
+        :rows="accountsTableData.rows"
+        :pagination-options="paginationOptions"
+        style-class="table"
+      >
+        <template #emptystate>
+          <div v-if="StormwaterStore.loadingStormwaterData">
+            Loading accounts... <font-awesome-icon
+              icon="fa-solid fa-spinner"
+              spin
+            />
+          </div>
+          <div v-else>
+            No accounts found
+          </div>
+        </template>
+        <template #pagination-top="props">
+          <custom-pagination-labels
+            :mode="'pages'"
+            :total="props.total"
+            :perPage="5"
+            @page-changed="props.pageChanged"
+            @per-page-changed="props.perPageChanged"
+          >
+          </custom-pagination-labels>
+        </template>
+      </vue-good-table>
+    </div>
+    <a
+      target="_blank"
+      :href="`https://stormwater.phila.gov/parcelviewer/parcel/${stormwaterData.Parcel.ParcelID}`"
+    >See more at Stormwater Billing <font-awesome-icon icon="fa-solid fa-external-link-alt" /></a>
+
+  </div>
 
 </template>
